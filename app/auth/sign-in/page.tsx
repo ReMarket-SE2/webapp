139b01185dcd6e74223f4ad509106a876bcb3876
matined <nedzimateusz@gmail.com>
2025-03-03 14:56:40 +0100

    feat(layout): create sidebar, header, and auth

---
+++ app/auth/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInForm } from "@/components/auth/sign-in-form"
+
+export default function SignInPage() {
+  return (
+    <div className="w-full max-w-xs">
+      <SignInForm />
+    </div>
+  )
+}
