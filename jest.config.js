87dff46ab922708f82ea3e2956b6f021f583c335
HermanPlay <hermanplay1110@gmail.com>
2025-03-27 12:33:34 +0100

    feat: coverage generation

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 // Providing the path to your Next.js app which will enable loading next.config.js and .env files
-const createJestConfig = nextJest({ dir: './' })
+const createJestConfig = nextJest({ dir: './' });
 
 // Any custom config you want to pass to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-  testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
+  ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
+    '^@/(.*)$': '<rootDir>/$1',
   },
-}
+  coverageReporters: [['text', { skipFull: true }], 'cobertura'],
+};
 
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
