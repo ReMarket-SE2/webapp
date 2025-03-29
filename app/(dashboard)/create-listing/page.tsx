cc840c81f0e7084e3c278a9952f98ce4a0fb8476
matined <nedzimateusz@gmail.com>
2025-03-29 17:01:25 +0100

    feat(listings): hook and form for creation of a new listing

--- app/(dashboard)/create-listing/page.tsx
+++ app/(dashboard)/create-listing/page.tsx
@@ -1,20 +1,9 @@
+import { CreateListingForm } from "@/components/listings/create-listing-form"
+
 export default function CreateListingPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="w-full h-full grid grid-cols-3 grid-rows-3 gap-4">
-        <div className="col-span-2 row-span-2">
-          <div className="w-full h-full bg-muted/50 rounded-xl" />
-        </div>
-        <div className="col-span-1 row-span-2">
-          <div className="w-full h-full bg-muted/50 rounded-xl" />
-        </div>
-        <div className="col-span-1 row-span-1">
-          <div className="w-full h-full bg-muted/50 rounded-xl" />
-        </div>
-        <div className="col-span-2 row-span-1">
-          <div className="w-full h-full bg-muted/50 rounded-xl" />
-        </div>
-      </div>
+    <div className="flex flex-1 flex-col p-6">
+      <CreateListingForm />
     </div>
   )
 }
