a48e8c00b80998c2bed0050af6a4cf57558a2dc7
matined <nedzimateusz@gmail.com>
2025-03-31 11:28:13 +0200

    fix: header button now redirects to /create-listing

--- components/header/create-listing-button.tsx
+++ components/header/create-listing-button.tsx
@@ -1,10 +1,14 @@
-import { cn } from "@/lib/utils";
-import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export function CreateListingButton({ className }: { className?: string }) {
-  return <Button variant="outline" className={cn("ml-auto h-8 cursor-pointer", className)}>
-    <Plus />
-    Sell your item
-  </Button>
+  return <Link href="/create-listing" className="ml-auto">
+    <Button variant="outline" className={cn("h-8 cursor-pointer", className)}>
+      <Plus />
+      Sell your item
+    </Button>
+  </Link>
 }
