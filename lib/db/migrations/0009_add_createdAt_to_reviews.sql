0457c3420336945acbb7eeef4f5df7d5aecf3226
Mateusz Polis <polismateusz@gmail.com>
2025-05-15 11:36:09 +0200

    chore(db): merge with master

--- lib/db/migrations/0009_add_createdAt_to_reviews.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "reviews" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "orders" ADD COLUMN "listing_id" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "orders" ADD CONSTRAINT "orders_listing_id_listings_id_fk" FOREIGN KEY ("listing_id") REFERENCES "public"."listings"("id") ON DELETE cascade ON UPDATE no action;
