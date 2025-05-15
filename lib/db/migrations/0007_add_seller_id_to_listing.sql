eff1eb47bd60dcbec10e1bffe9a7f5416572fd8c
Mateusz Polis <polismateusz@gmail.com>
2025-05-15 11:03:46 +0200

    chore(db): fix migrations

--- lib/db/migrations/0007_add_seller_id_to_listing.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "listings" ADD COLUMN "seller_id" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "listings" ADD CONSTRAINT "listings_seller_id_users_id_fk" FOREIGN KEY ("seller_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
